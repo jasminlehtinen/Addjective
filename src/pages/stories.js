2b18488aae63e6a94a5693b7543e9a753cbdcbde
jasminlehtinen <jasmin.lehtinen@live.fi>
2022-07-06 10:50:26 +0300

    Working on showing the adjectives

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 const Stories = () => {
   const { id } = useParams()
   const [textarea, setTextarea] = useState('')
+  const [show, setShow] = useState([])
 
   const handleChange = (event) => {
     setTextarea(event.target.value)
@@ -13,12 +14,13 @@ const Stories = () => {
     event.preventDefault()
     const data = document.getElementById('data')
     console.log(data.value)
+    setShow(data.value)
   }
 
-  const SwitchStory = ({ page }) => {
+  const SwitchStory = ({ page, show }) => {
     switch(page) {
       case '1':
-        return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi scelerisque condimentum viverra. Fusce vel efficitur elit.'
+        return <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi scelerisque condimentum viverra. Fusce vel efficitur elit.</div>
       case '2':
         return 'Donec non justo congue, pretium orci vel, consectetur lacus. Quisque velit tellus, cursus eu sem et, tempus porttitor purus.'
       case '3':
@@ -31,7 +33,7 @@ const Stories = () => {
   return (
     <>
       <div className='content'>
-        <SwitchStory page={id}/>
+        <SwitchStory page={id} show={show}/>
         <form noValidate onSubmit={handleSubmit}>
           <textarea 
             type='text' 
@@ -40,6 +42,7 @@ const Stories = () => {
             value={textarea} 
             onChange={handleChange} />
           <button type='submit'>Lisää</button>
+          <p>{show}</p>
         </form>
       </div>
     </>

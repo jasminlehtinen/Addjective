a3074875e0958309eb73c8b69c08cbd4768785f5
jasminlehtinen <jasmin.lehtinen@live.fi>
2022-07-07 20:11:19 +0300

    Adjectives now show up in the story
    Stories still work in progress, used an existing story for now.

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -5,6 +5,7 @@ const Stories = () => {
   const { id } = useParams()
   const [textarea, setTextarea] = useState('')
   const [show, setShow] = useState([])
+  let showArray = show.toString().split(',')
 
   const handleChange = (event) => {
     setTextarea(event.target.value)
@@ -20,7 +21,17 @@ const Stories = () => {
   const SwitchStory = ({ page, show }) => {
     switch(page) {
       case '1':
-        return <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi scelerisque condimentum viverra. Fusce vel efficitur elit.</div>
+        return <div>
+                <p>Olipa kerran {show[0]} talvi. Kuningatar istui linnan ikkunan ääressä ja katseli maahan putoilevia {show[2]} lumihiutaleita.
+                "Voi, olisipa minulla lapsi", kuningatar toivoi.</p>
+                <p>Pian toive toteutuikin ja kuningatar sai tytön. Tyttö oli {show[1]}, ja hänen ihonsa oli {show[3]} kuin lumi. Tyttö sai nimekseen Lumikki.</p>
+                <p>Valitettavasti kuningatar, Lumikin oikea äiti, kuoli pian. Kuningas meni uudelleen naimisiin, ja Lumikki sai uuden äidin. 
+                Tämä oli {show[5]} ja {show[4]} nainen.</p>
+                <p>Lumikin uusi äiti oli nyt maan kuningatar. Hän halusi olla myös maan {show[7]} nainen.</p>
+                <p>Kuningattarella oli taikapeili, josta hän katseli itseään. Kuningatar kysyi peililtä: "Kerro, kerro, kuvastin, ken on maassa {show[8]}."
+                  Peili vastasi kuningattarelle: "Sinä olet {show[6]}."
+                </p>
+               </div>
       case '2':
         return 'Donec non justo congue, pretium orci vel, consectetur lacus. Quisque velit tellus, cursus eu sem et, tempus porttitor purus.'
       case '3':
@@ -33,7 +44,7 @@ const Stories = () => {
   return (
     <>
       <div className='content'>
-        <SwitchStory page={id} show={show}/>
+        <SwitchStory page={id} show={showArray}/>
         <form noValidate onSubmit={handleSubmit}>
           <textarea 
             type='text' 

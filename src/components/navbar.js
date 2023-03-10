586792a97bd6bf3afd3f043a6b9cc919aeb3db4e
jasminlehtinen <jasmin.lehtinen@live.fi>
2023-03-10 14:12:46 +0200

    Fixed active links
    Now navbar shows which story you have selected

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,28 +1,37 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './navbar.css'
 
 const Navbar = () => {
   return (
     <>
-      <div className='navbar--flexbox navbar--background'>
+      <nav className='navbar--flexbox navbar--background'>
         <div>
-          <li                  >
-            <Link to='/' className='link--decoration link--decoration__big'>Addjective</Link>
+          <li>
+            <NavLink to='/' className='link--decoration link--decoration__big'>Addjective</NavLink>
           </li>
         </div>
         <div className='links--flexbox'>
           <li>
-            <Link to='/tarina/1' className='link--decoration link--decoration__small'>Tarina 1</Link>
+            <NavLink
+              to='/tarina/1' 
+              className={({ isActive }) => isActive ? 'link--decoration link--decoration__small link--decoration__underline' : 'link--decoration link--decoration__small'}
+            >Tarina 1</NavLink>
           </li>
           <li>
-            <Link to='/tarina/2' className='link--decoration link--decoration__small'>Tarina 2</Link>
+            <NavLink 
+              to='/tarina/2' 
+              className={({ isActive }) => isActive ? 'link--decoration link--decoration__small link--decoration__underline' : 'link--decoration link--decoration__small'}
+            >Tarina 2</NavLink>
           </li>
           <li>
-            <Link to='/tarina/3' className='link--decoration link--decoration__small'>Tarina 3</Link>
+            <NavLink 
+              to='/tarina/3' 
+              className={({ isActive }) => isActive ? 'link--decoration link--decoration__small link--decoration__underline' : 'link--decoration link--decoration__small'}
+            >Tarina 3</NavLink>
           </li>
         </div>
-      </div>
+      </nav>
     </>
   )
 }

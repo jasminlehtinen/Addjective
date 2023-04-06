1c0495f48cec059c767121446b92a0f59d03265f
jasminlehtinen <jasmin.lehtinen@live.fi>
2023-04-06 11:14:35 +0300

    Home page changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,11 +5,13 @@ const Home = () => {
   return (
     <>
       <div className='home'>
-        <p>Tervetuloa! Addjective on omanlaisensa tarinageneraattori, jossa voit lisätä omia adjektiiveja tuttuihin lasten satuihin. 
-            Tämä sivusto sopii niille, jotka haluavat lisätä oman mausteensa perinteisiin satuihin, ja tehdä niistä vieläkin mielenkiintoisempia ja hauskempia. 
-            Anna siis mielikuvituksesti lentää vapaasti! 
-        </p>
-        <p>Valittavissa on tällä hetkellä kolme eri satua, jotka löydät sivuston oikeasta yläkulmasta.</p>
+        <div className='home__paragraphs'>
+          <p>Tervetuloa! Addjective on omanlaisensa tarinageneraattori, jossa voit lisätä omia adjektiiveja tuttuihin lasten satuihin. 
+              Tämä sivusto sopii niille, jotka haluavat lisätä oman mausteensa perinteisiin satuihin, ja tehdä niistä vieläkin mielenkiintoisempia ja hauskempia. 
+              Anna siis mielikuvituksesti lentää vapaasti! 
+          </p>
+          <p>Valittavissa on tällä hetkellä kolme eri satua, jotka löydät sivuston oikeasta yläkulmasta.</p>
+        </div>
       </div>
     </>
   )
